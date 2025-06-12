2e68a2f61410f6a3d6b69b1a6d2d4f7b2acc1782
yvarevaloh <yvarevaloh10@gmail.com>
2025-06-11 22:31:14 -0500

    Add main.py with pandas import as pd

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+import pandas as pd
+import tensorflow as tfs
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, SmartNinja!"
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
